846807ec032b1c5f9627d5796a5303de8d7367d0
SatsArts <satsarts@gmail.com>
2020-03-16 22:18:04 +0100

    arg fix.

--- src/components/atoms/Buttons/ButtonSmall.js
+++ src/components/atoms/Buttons/ButtonSmall.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'react-native';
 
-const ButtonSmall = ({title}, {onPress = () => {}}) => <Button title={ title } onPress={onPress} />
+const ButtonSmall = ({title, onPress}) => <Button title={ title } onPress={onPress} />
 
 export default ButtonSmall;
