d65313facc94751f06cac53b1f57054f06b52807
Filip Marklars <filip.marklars@soleilit.se>
2020-03-19 12:54:47 +0100

    ge mig contrib!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, SafeAreaView, Button } from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
 
 //router
 import Tabs from './router';
@@ -9,18 +16,38 @@ import TempUser from './lib/user';
 
 function getTempUser() {
   return fetch('/lib/user')
-    .then((response) => response.json())
-    .then((responseJson) => {
+    .then(response => response.json())
+    .then(responseJson => {
       console.log(responseJson);
     })
-    .catch((error) => {
+    .catch(error => {
       console.error(error);
     });
 }
 
 const App = () => (
   <SafeAreaView style={{flex: 1}}>
-    <Button title="PRess me" onPress={() => this.getTempUser}/>
+    <StatusBar backgroundColor="#333" barStyle="light-content" />
+    <SafeAreaView style={styles.container}>
+      <View style={[{flexDirection: 'row'}, styles.item]}>
+        <View style={[{backgroundColor: '#EE2C38'}, styles.item]}>
+          <Text p style={styles.text}>
+            credits
+          </Text>
+        </View>
+        <View style={[{backgroundColor: '#Ab0672'}, styles.item]}>
+          <Text p style={styles.text}>
+            info
+          </Text>
+        </View>
+        <View style={[{backgroundColor: '#542165'}, styles.item]}>
+          <Text p style={styles.text}>
+            sessiontime
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+    <Button title="PRess me" onPress={() => this.getTempUser} />
     <View style={{flex: 1}}>
       <Tabs />
     </View>
@@ -28,3 +55,25 @@ const App = () => (
 );
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flexWrap: 'nowrap',
+    height: 25,
+  },
+  elementsContainer: {
+    backgroundColor: '#ecf5fd',
+    marginTop: 48,
+  },
+  item: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  text: {
+    textAlign: 'center',
+    color: '#fff',
+  },
+});
