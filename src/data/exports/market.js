c127f5896906fd13c119bf1a619e14d9ece02bc4
Filip Marklars <filip.marklars@soleilit.se>
2020-04-12 01:16:29 +0200

    påbörjat market

---
+++ src/data/exports/market.js
@@ -0,0 +1,87 @@
+const marketCases = [
+  {
+    name: 'GOLD',
+    image:
+      'https://vignette.wikia.nocookie.net/piratepower/images/3/32/Icon_Gold_Chest.png/revision/latest/scale-to-width-down/340?cb=20160313142816',
+  },
+  {
+    name: 'ANCIENT',
+    image: 'https://i.dlpng.com/static/png/756977_thumb.png',
+  },
+  {
+    name: 'MYSTERY',
+    image:
+      'https://gamepedia.cursecdn.com/smite_gamepedia/thumb/8/88/TreasureRoll_Mystery.png/256px-TreasureRoll_Mystery.png?version=29627397fc3b65b4a11c079866661c50',
+  },
+];
+
+const marketItems = [
+  {
+    name: 'Handbag',
+    image:
+      'https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/bag-icon.png',
+    rarity: 'green',
+    score: 120,
+  },
+  {
+    name: 'Sportsbag',
+    image:
+      'https://cdn2.iconfinder.com/data/icons/sports-and-games-3-3/48/147-512.png',
+    rarity: 'blue',
+    score: 310,
+  },
+  {
+    name: 'Briefcase',
+    image: 'https://www.iconhot.com/icon/png/glossd/512/briefcase-3.png',
+    rarity: 'green',
+    score: 80,
+  },
+
+  {
+    name: 'Hat',
+    image:
+      'https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/hat.png',
+    rarity: 'gray',
+    score: 90,
+  },
+  {
+    name: 'Jeans',
+    image:
+      'https://cdn3.iconfinder.com/data/icons/fashion-category-color/100/Denim-512.png',
+    rarity: 'white',
+    score: 130,
+  },
+  {
+    name: 'T-shirt',
+    image:
+      'https://iconsgalore.com/wp-content/uploads/2018/10/t-shirt-1-featured.png',
+    rarity: 'pink',
+    score: 150,
+  },
+
+  {
+    name: 'Crocs',
+    image:
+      'https://cdn2.iconfinder.com/data/icons/clothes-shoes-vol-1/64/crocs-512.png',
+    rarity: 'purple',
+    score: 210,
+  },
+  {
+    name: 'Sandals',
+    image:
+      'https://www.creativefabrica.com/wp-content/uploads/2019/05/Sandals-icon-vector-by-hellopixelzstudio.png',
+    rarity: 'orange',
+    score: 230,
+  },
+  {
+    name: 'Sneakers',
+    image:
+      'https://cdn.iconscout.com/icon/premium/png-256-thumb/sneakers-391-457045.png',
+    rarity: 'yellow',
+    score: 420,
+  },
+];
+
+const market = {marketCases, marketItems};
+
+export default market;
--- src/scenes/views/Market.js
+++ src/scenes/views/Market.js
@@ -1,9 +1,83 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Button, Text} from 'react-native';
 
-const Market = () => (
-  <View>
-    <Text>Market screen</Text>
+// Styles
+import main from '_styles';
+
+import market from '../../data/exports/market';
+
+//Components
+import CaseBox from '../../components/global/caseBox';
+import ItemBox from '../../components/global/itemBox';
+
+const Market = props => (
+  <View style={main.scenes.home.container}>
+    <View style={{marginBottom: 15, marginTop: 15}}>
+      <Text
+        style={{
+          color: '#fff',
+          fontSize: 16,
+          paddingHorizontal: 15,
+          paddingVertical: 10,
+        }}>
+        BEST CASE OFFERS
+      </Text>
+      <View
+        style={{
+          paddingVertical: 8,
+          paddingHorizontal: 15,
+          backgroundColor: '#484851',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+        }}>
+        {market.marketCases.map((obj, index) => (
+          <CaseBox
+            case={obj}
+            width={'31%'}
+            marginRight={0}
+            marginBottom={12}
+            paddingTop={3}
+            key={index}
+          />
+        ))}
+      </View>
+    </View>
+    <View style={{marginBottom: 10}}>
+      <Text
+        style={{
+          color: '#fff',
+          fontSize: 16,
+          paddingHorizontal: 15,
+          paddingVertical: 10,
+        }}>
+        BEST ITEM OFFERS
+      </Text>
+      <View
+        style={{
+          paddingVertical: 8,
+          paddingHorizontal: 15,
+          backgroundColor: '#484851',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          width: '100%',
+        }}>
+        {market.marketItems.map((obj, index) => (
+          <ItemBox
+            item={obj}
+            width={'23%'}
+            marginBottom={15}
+            marginRight={'2%'}
+            borderColor={obj.rarity}
+            key={index}
+          />
+        ))}
+      </View>
+    </View>
   </View>
 );
 
