e3177b8726c515a7ff6bad3225c22b8d8b82427e
Filip Marklars <filip.marklars@soleilit.se>
2020-04-19 15:09:57 +0200

    mer caseopening-animation shit

--- src/components/stash/openCase.js
+++ src/components/stash/openCase.js
@@ -1,5 +1,13 @@
 import React, {useRef} from 'react';
-import {Animated, Text, View, StyleSheet, Button} from 'react-native';
+import {
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Alert,
+  Dimensions,
+} from 'react-native';
 
 import ItemBox from '../global/itemBox';
 
@@ -7,21 +15,61 @@ import exampleStorage from '../../data/exports/exampleUserStorage';
 
 const randomCase = exampleStorage.exampleGeneratedCase;
 
+const screenWidth = Math.round(Dimensions.get('window').width);
+const boxWidth = screenWidth * 0.35;
+const randomPlacement = Math.floor(Math.random() * boxWidth) + 1;
+
 export default function App() {
+  console.log(screenWidth);
+  console.log(randomPlacement);
   // fadeAnim will be used as the value for opacity. Initial Value: 0
-  const leftAnim = useRef(new Animated.Value(60)).current;
+  const leftAnim = useRef(new Animated.Value(screenWidth / 2)).current;
 
   const moveLeft = () => {
     // Will change fadeAnim value to 1 in 5 seconds
     Animated.timing(leftAnim, {
-      toValue: -2000,
+      toValue: -(screenWidth * 9.88 + randomPlacement),
       duration: 6000,
-    }).start();
+    }).start(() => {
+      test();
+    });
+
+    console.log('animation started');
   };
+
+  function test() {
+    console.log('animation done!');
+    Alert.alert(
+      'Congratulations! You found ',
+      randomCase[28].name,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+    );
+  }
+
+  console.log(randomCase);
+  console.log(randomCase[27]);
 
   return (
     <View style={{flex: 1, backgroundColor: '#33333D'}}>
-      <View style={{zIndex: 2, position: 'absolute', width: 5, height: 20, left: '50%', top: 145, backgroundColor: '#ffff00'}} />
+      <View
+        style={{
+          zIndex: 2,
+          position: 'absolute',
+          width: 5,
+          height: 20,
+          left: '50%',
+          top: '25%',
+          backgroundColor: '#ffff00',
+        }}
+      />
       <Animated.View
         style={[
           styles.container,
@@ -32,7 +80,7 @@ export default function App() {
         {randomCase.map((obj, index) => (
           <ItemBox
             item={obj}
-            width={'23%'}
+            width={'35%'}
             marginBottom={15}
             marginRight={'2%'}
             borderColor={obj.rarity}
